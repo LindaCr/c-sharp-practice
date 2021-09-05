3e322f5295b63885cafb08b0ede8add59f9de9f0
LindaCr <lindacrook.44@gmail.com>
2021-09-04 22:11:26 -0400

    init commit

---
+++ calculator-practice/calculator-practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace calculator_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola!");
+
+            string choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("Enter loan amount: ");
+                string amountStr = Console.ReadLine();
+                double amount = double.Parse(amountStr);
+
+                Console.WriteLine("Enter interest amount: ");
+                string aprStr = Console.ReadLine();
+                double apr = double.Parse(aprStr);
+
+                double interest = amount * apr;
+                Console.WriteLine("Interest: " + interest);
+
+
+                Console.WriteLine();
+                Console.WriteLine("Continue? y/n");
+                choice = Console.ReadLine();
+            }
+
+
+
+            Console.WriteLine("Peace");
+        }
+    }
+}
