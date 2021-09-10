c546273fe3f5b673a0f32f74e2d3fe7225e205c7
LindaCr <lindacrook.44@gmail.com>
2021-09-09 22:16:47 -0400

    init commit

---
+++ squares/squares/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Squares and Cubes Table!");
+            Console.WriteLine();
+
+            string choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("Enter an integer: ");
+                int nbr = Convert.ToInt32(Console.ReadLine());
+
+                int square = nbr * nbr;
+                int cube = nbr * nbr * nbr;
+
+                Console.WriteLine();
+                Console.WriteLine("Number    Square     Cube");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine(" " + nbr + "        " + square + "         " + cube);
+
+
+                Console.WriteLine();
+                Console.WriteLine("Continue? y/n");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Peace");
+        }
+    }
+}
