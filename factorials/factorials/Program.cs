1e282f8df6d27e1d0b3b156586e127590303e7b7
LindaCr <lindacrook.44@gmail.com>
2021-09-09 22:16:19 -0400

    init commit

---
+++ factorials/factorials/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace factorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			Console.WriteLine("Welcome to the Factorial Calculator");
+			Console.WriteLine();
+
+			String choice = "y";
+			while (choice.Equals("y"))
+			{
+
+				Console.WriteLine("Enter an integer that's greater than 0 and less than 10:");
+				int number = Convert.ToInt32(Console.ReadLine());
+
+				long factorial = 1;
+				for (int i = 1; i <= number; i++)
+				{
+					factorial *= i;
+				}
+				Console.WriteLine("The factorial of " + number + " is " + factorial);
+
+				Console.WriteLine();
+				Console.WriteLine("Continue? y/n");
+				choice = Console.ReadLine();
+			}
+			Console.WriteLine();
+			Console.WriteLine("Peace");
+		}
+    }
+}
